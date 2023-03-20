91f90171575c51e6c55a04ed8e71c57f3b3c2d8b
domindez <109239601+domindez@users.noreply.github.com>
2023-03-20 22:46:16 +0100

    algorithym

--- logic/createMap.ts
+++ logic/createMap.ts
@@ -17,18 +17,16 @@ export const createPath = () => {
     }
   }
 
-  const path: number[][] = [initialPos, [10, 6]]
+  const path: number[][] = [initialPos]
 
   const lastCell = path[path.length - 1]
 
   // Decidir en qué direccion va el siguiente paso
   const availableDirections: string[] = []
-  if (lastCell[y] !== 0 && checkNotAdjacent(tablero, path, lastCell, 'up')) availableDirections.push('up')
-  if (lastCell[y] !== alto - 1 && checkNotAdjacent(tablero, path, lastCell, 'down')) availableDirections.push('down')
-  if (lastCell[x] !== 0 && checkNotAdjacent(tablero, path, lastCell, 'left')) availableDirections.push('left')
-  if (lastCell[x] !== ancho - 1 && checkNotAdjacent(tablero, path, lastCell, 'right')) availableDirections.push('right')
-
-  console.log('object')
+  if (lastCell[y] !== 0 && checkValid(tablero, path, lastCell, 'up')) availableDirections.push('up')
+  if (lastCell[y] !== alto - 1 && checkValid(tablero, path, lastCell, 'down')) availableDirections.push('down')
+  if (lastCell[x] !== 0 && checkValid(tablero, path, lastCell, 'left')) availableDirections.push('left')
+  if (lastCell[x] !== ancho - 1 && checkValid(tablero, path, lastCell, 'right')) availableDirections.push('right')
 
   const direction = availableDirections[Math.floor(Math.random() * availableDirections.length)]
 
@@ -58,7 +56,7 @@ export const createPath = () => {
   return path
 }
 
-const checkNotAdjacent = (tablero: number[][][], path: number[][], lastCell: number[], direction: string) => {
+const checkValid = (tablero: number[][][], path: number[][], lastCell: number[], direction: string) => {
   let valid = true
 
   const newCell = [...lastCell]
@@ -86,50 +84,47 @@ const checkNotAdjacent = (tablero: number[][][], path: number[][], lastCell: num
   }
 
   console.log('newCell :>> ', newCell)
+  console.log('lastCell :>> ', lastCell)
+  if (newCell[y] === lastCell[y] && newCell[x] === lastCell[x]) {
+    valid = false
+  }
 
   // Celdas adyacentes
-  const cell1 = tablero[newCell[y] - 1][newCell[x]]
-  const cell2 = tablero[newCell[y] + 1][newCell[x]]
-  const cell3 = tablero[newCell[y]][newCell[x] - 1]
-  const cell4 = tablero[newCell[y]][newCell[x] + 1]
+  const cell1 = newCell[y] - 1 >= 0 ? tablero[newCell[y] - 1][newCell[x]] : null
+  const cell2 = newCell[y] + 1 < tablero.length ? tablero[newCell[y] + 1][newCell[x]] : null
+  const cell3 = newCell[x] - 1 >= 0 ? tablero[newCell[y]][newCell[x] - 1] : null
+  const cell4 = newCell[x] + 1 < tablero[0].length ? tablero[newCell[y]][newCell[x] + 1] : null
 
-  const cellsToCheck = [cell1, cell2, cell3, cell4]
+  let cellsToCheck = [cell1, cell2, cell3, cell4]
 
-  // Elimina de las adyacentes los undefinidos
+  // Comprueba que las adyacentes no sean nulas
+  cellsToCheck = cellsToCheck.filter(cellToCheck => cellToCheck !== null)
+
+  // Elimina la última celda
   cellsToCheck.forEach(element => {
     const indexToDelete = cellsToCheck.indexOf(element)
-    if (element === undefined) cellsToCheck.splice(indexToDelete, 1)
+    if (element !== null) {
+      if (element[y] === lastCell[y] && element[x] === lastCell[x]) cellsToCheck.splice(indexToDelete, 1)
+    }
   })
-
-  // Elimina de las adyacentes la celda anterior --- ESTO ESTA MAL, LA ANTERIOR NO PUEDE ESTAR
-  // cellsToCheck.forEach(cell => {
-  //   if (cell[y] === lastCell[y] && cell[x] === lastCell[x]) {
-  //     const indexToDelete = cellsToCheck.indexOf(cell)
-  //     cellsToCheck.splice(indexToDelete, 1)
-  //   }
-  // })
 
   // HACER QUE EL PRIMER FALSE RETORNE
 
   // Comprueba que las adyacentes no están en el path
   cellsToCheck.forEach(cellToCheck => {
-    path.forEach(cellInPath => {
-      if (cellToCheck[y] === cellInPath[y] && cellToCheck[x] === cellInPath[x]) valid = false
-    })
+    if (cellToCheck !== null) {
+      path.forEach(cellInPath => {
+        if (cellToCheck[y] === cellInPath[y] && cellToCheck[x] === cellInPath[x]) valid = false
+      })
+    }
   })
 
-  // Comprueba que la celda no está en el path
-  path.forEach(cellsInPath => {
-    cellsInPath.forEach(cellInPath => {
-      if (newCell[y] === cellInPath[y] && newCell[x] === cellInPath[x]) valid = false
-    })
+  const isDuplicate = path.some(cellInPath => {
+    return cellInPath[y] === newCell[y] && cellInPath[x] === newCell[x]
   })
-
-  console.log('path :>> ', path)
-  console.log('direction :>> ', direction)
-  console.log('newCell :	>> ', newCell)
-  console.log('cellsToCheck :>> ', cellsToCheck)
-  console.log('valid :>> ', valid)
+  if (isDuplicate) {
+    valid = false
+  }
 
   return valid
 }
